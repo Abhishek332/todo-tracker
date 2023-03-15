7346c8a306b9e207c4629bbd749751ee801dd78c
abhishek332 <abhishek.porwal@deuexsolutions.com>
2023-03-15 12:35:35 +0530

    add paragraph instead of textarea to render todo

--- script.js
+++ script.js
@@ -72,7 +72,9 @@ class ScreenHandler {
 			(TodoListItem) => `
             <form class="todo-list-item" id="${TodoListItem.id}">
                 <input type="checkbox" ${TodoListItem.isCompleted ? 'checked' : ''}/>
-                <textarea disabled  ${TodoListItem.isCompleted ? 'class="mark-done"': ''}>${TodoListItem.todoText}</textarea>
+                <div class="todo-text">
+					<p  ${TodoListItem.isCompleted ? 'class="mark-done"': ''}>${TodoListItem.todoText}</p>
+				</div>
                 <button data-id="edit-todo-btn"><i class="fa-solid fa-pen-to-square edit-btn"></i></button>
 				<button data-id="delete-todo"><i class="fa-solid fa-xmark"></i></button>
             </form>
@@ -101,24 +103,25 @@ class ScreenHandler {
 			});
 
 			CheckBox.addEventListener('click', (e) => {
-                console.log(e.target.checked)
                 TodosList.updateTodoStatus(TodoListItem, e.target.checked);
             })
 
 			EditBtn.addEventListener('click', (e) => {
 				Todo.removeChild(EditBtn);
 				Todo.removeChild(DeleteBtn);
+				const TodoTextBlock = Todo.querySelector('.todo-text');
+				TodoTextBlock.innerHTML = `<textarea class="todo-text-input">${TodoListItem.todoText}</textarea>`;
 				Todo.innerHTML += `<button data-id="save-updated-todo">
 										<i class="fa-solid fa-floppy-disk"></i>
 									</button>
 									<button data-id="delete-todo">
 										<i class="fa-solid fa-xmark"></i>
 									</button>`;
-				Todo.querySelector('textarea').removeAttribute('disabled');
 				Todo.querySelector('textarea').focus();
 			});
 
 			DeleteBtn.addEventListener('click', (e) => {
+				e.preventDefault();
 				TodosList.deleteTodo(TodoListItem);
 			});
 		});
